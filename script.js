2791edf4d5ab0cbf944b64432b350c44e8b5ff7f
richiegecko <richie.hickey513@gmail.com>
2022-04-13 15:38:41 -0300

    Added button to clear grid of color

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-const sketchBox = document.querySelector('.container')
-let box;
+const sketchBox = document.querySelector('.container');
+const clear = document.querySelector('.erase');
+const sizes = document.querySelectorAll('.size');
 
-for (i=0; i<256; i++){
-    box = document.createElement('div')
+let gridSize = 16
+
+for (i=0; i<gridSize**2; i++){
+    const box = document.createElement('div')
     box.className = 'box';
-    box.setAttribute('style','border:1px solid #E5E4E2;')
     sketchBox.appendChild(box)
 }
+let divList = document.querySelectorAll('.box')
 
+function erase(e) {
+  divList.forEach((div) => {
+    div.style.backgroundColor = "white";
+  });
+}
 
+clear.addEventListener('click', erase)
+
+function color() {
+  divList.forEach((div) => {
+    div.onmouseover = function(e) {
+      e.target.setAttribute('style','background-color:seagreen')
+    }
+    
+  })
+}
+
+color()
